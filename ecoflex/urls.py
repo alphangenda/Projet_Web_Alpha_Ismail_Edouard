bb9d857702c67797844440f8b9939183445aa56d
Ismail Batoul <isbat001@edu.cegepgarneau.ca>
2025-09-30 20:32:48 -0400

    Ajout de logique pour la connexion de l'admin

--- ecoflex/urls.py
+++ ecoflex/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profil/', views.profil, name='profil'),
     path('connexion/', views.connexion, name='connexion'),
     path('inscription/', views.inscription, name='inscription'),
+    path('gestion_users/', views.gestion_users, name='gestion_users')
 ]
