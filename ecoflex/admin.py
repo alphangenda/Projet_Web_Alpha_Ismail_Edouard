c130a43b62e3931553de48a9c734d893c39a69ea
Alpha-Trésor Ngendanduwme <ngendandumwea6@gmail.com>
2025-09-30 22:45:40 -0400

    Création du modele et la classe User pour l'interface admin

--- ecoflex/admin.py
+++ ecoflex/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.site_header = "Administration EcoFlex"
+admin.site.site_title = "EcoFlex Admin"
+admin.site.index_title = "Gestion de la plateforme de mobilité"
+
+@admin.register(User)
+class UserAdministration(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Statut du compte', {'fields': ('is_active',)}),
+    )
--- ecoflex/models.py
+++ ecoflex/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Meta:
+        verbose_name = "Utilisateur"
+        verbose_name_plural = "Utilisateurs"
+
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        elif self.first_name:
+            return self.first_name
+        else:
+            return self.username
