32cb12dd7c5301c332837b808907fa3ef9e2f780
EdouardGE <creamsyko@gmail.com>
2025-10-01 08:22:39 -0400

    Modification de la classe user pour ui permettre davoir des autres champs

--- ecoflex/models.py
+++ ecoflex/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    province = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+
     class Meta:
         verbose_name = "Utilisateur"
         verbose_name_plural = "Utilisateurs"
