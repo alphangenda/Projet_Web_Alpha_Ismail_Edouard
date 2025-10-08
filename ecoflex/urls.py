2dd211e631ef319e09cd57920ea66756e1746edc
EdouardGE <creamsyko@gmail.com>
2025-10-07 21:42:27 -0400

    Ajout de la page map_location ainsi que lintegration dune map interactive avec leaflet centrée sur la ville de Québec : configuration du fond de carte Carto Light et ajout d’un marqueur de test sur Québec. Modification du fichier models.py et admin.py pour integrer le modele Station et son interface admin

--- ecoflex/urls.py
+++ ecoflex/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.accueil, name='accueil'),
     path('tarif/', views.tarif, name='tarif'),
     path('fonctionnement/', views.fonctionnement, name='fonctionnement'),
+    path('map_location/', views.map_location, name='map_location'),
     path('profil/', views.profil, name='profil'),
     path('connexion/', views.connexion, name='connexion'),
     path('deconnexion/', views.deconnexion, name='deconnexion'),
@@ -12,6 +13,8 @@ urlpatterns = [
     path('gestion_users/', views.gestion_users, name='gestion_users'),
     path('action_status/<int:user_id>/', views.action_status, name='action_status'),
     path('profil/modifier/', views.modifier_profil, name='modifier_profil'),
+
+
 
 
 
--- ecoflex/views.py
+++ ecoflex/views.py
@@ -21,6 +21,9 @@ def tarif(request):
 def fonctionnement(request):
     return render(request, 'ecoflex/fonctionnement.html')
 
+def map_location(request):
+    return render(request, 'ecoflex/map_location.html')
+
 def profil(request):
     return render(request, 'ecoflex/profil.html')
 
